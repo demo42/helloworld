7eb197825598fcb5edc48bc71e99fd259eff3ff3
Steve Lasker <StevenLasker@Hotmail.com>
2018-06-07 09:04:55 -0700

    refactor release to deploy project

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var port = 80
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.write('<style type="text/css">body {background-color:'+process.env.BACKGROUND_COLOR+';}</style>');
-  response.write('<h1>A fancy website</h1>');
+  response.write('<h1>Azure Container Registry Build</h1>');
+  response.write('<h3>Enabling OS & Framework Patching</h3>');
+  response.write('<a href="https://aka.ms.acr/build">https://aka.ms.acr/build</a>');
   response.write('<p>Hello Azure Friday</p>');
   response.write('<p><b>Version:</b> '+process.env.NODE_VERSION+'</p>');
   response.end();
