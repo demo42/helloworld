68a0b275048d2550f5edd4f8b055ba1e378b6f22
Steve Lasker <StevenLasker@Hotmail.com>
2018-08-21 13:03:01 -0700

    fix background colors

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var port = 80
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('<style type="text/css">body {background-color:red;}</style>');
+  response.write('<style type="text/css">body {background-color:'+process.env.BACKGROUND_COLOR+';}</style>');
   response.write('<h1>Azure Container Registry Tasks</h1>');
   response.write('<h3>Enabling OS & Framework Patching</h3>');
   response.write('<a href="https://aka.ms/acr/build">https://aka.ms.acr/build</a>');
-  response.write('<p>Hello world</p>');
+  response.write('<p>Hello world!</p>');
   response.write('<p><b>Version:</b> '+process.env.NODE_VERSION+'</p>');
   response.write('<p><b>HostName:</b> '+os.hostname()+'</p>');
   
