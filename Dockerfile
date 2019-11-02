795c05deaaf30861af24e9eb41e917944d9cc8fb
Steve Lasker <StevenLasker@Hotmail.com>
2019-11-02 13:37:53 -0700

    KubeCon Demo Updates

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM demo42.azurecr.io/baseimages/node:9-alpine
+ARG ACR_NAME=demo42t
+FROM ${ACR_NAME}.azurecr.io/base-images/node:9-alpine
 EXPOSE 80
 COPY . /src 
 RUN cd /src && npm install
--- env.sh
+++ env.sh
@@ -2,18 +2,22 @@
 
 # Replace these values for your configuration
 # I've left our values in, as we use this for our demos, providing some examples
-export ACR_NAME=demo42
+export ACR_NAME=demo42t
 export RESOURCE_GROUP=$ACR_NAME
-export RESOURCE_GROUP_LOCATION=eastus
+export RESOURCE_GROUP_LOCATION=southcentralus
 export REGISTRY_NAME=${ACR_NAME}.azurecr.io/ 
 
+export AKS_NAME=demo42-dev
+export AKS_RG_NAME=demo42-dev-scus
+
 # App service Configuration
-export APP_SERVICE_URL=https://demo42-eus.azurewebsites.net
+export APP_SERVICE_URL=https://demo42-scus.azurewebsites.net
 
 # Cloned Github Repo
 export GIT_REPO=https://github.com/demo42/helloworld.git
 
 # Azure Keyvault Name
-export AKV_NAME=$ACR_NAME 
+export AKV_NAME=demo42
 # Token Name within Keyvault
 export GIT_TOKEN_NAME=demo42-git-token 
+
