1747f3806bebef34df3f86a70cf41568a6b3bb97
Steve Lasker <StevenLasker@Hotmail.com>
2019-11-02 14:46:47 -0700

    Change from images to artifacts

--- env.sh
+++ env.sh
@@ -15,6 +15,7 @@ export APP_SERVICE_URL=https://demo42-scus.azurewebsites.net
 
 # Cloned Github Repo
 export GIT_REPO=https://github.com/demo42/helloworld.git
+export GIT_BASE_IMAGE_NODE=https://github.com/demo42/baseimage-node.git
 
 # Azure Keyvault Name
 export AKV_NAME=demo42
