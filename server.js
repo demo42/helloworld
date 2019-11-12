8509e612f0f5e79535a54fb25f1782fbd40ce6dc
steve lasker <stevenlasker@hotmail.com>
2019-11-11 20:31:53 -0800

    .

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var port = 80
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
+ 
   response.write('<style type="text/css">body {background-color:'+process.env.BACKGROUND_COLOR+';}</style>');
+ 
   response.write('<h1>Azure Container Registry Tasks</h1>');
   response.write('<h3>Enabling OS & Framework Patching</h3>');
   response.write('<a href="https://aka.ms/acr/tasks">https://aka.ms.acr/tasks</a>');
