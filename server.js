17bb005b44f9675bf9b8502af59d76d5a37f60e5
steve lasker <stevenlasker@hotmail.com>
2019-11-16 18:47:35 -0800

    .

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ var server = http.createServer(function (request, response) {
  
   response.write('<h1>Azure Container Registry Tasks</h1>');
   response.write('<h3>Enabling OS & Framework Patching</h3>');
-  response.write('<a href="https://aka.ms/acr/tasks">https://aka.ms/acr/tasks</a>');
-  response.write('<a href="https://aka.ms/teleport/signup">https://aka.ms/teleport/signup</a>');
+  response.write('<a href="https://aka.ms/acr/tasks">https://aka.ms/acr/tasks</a></p>');
+  response.write('<a href="https://aka.ms/teleport/signup">https://aka.ms/teleport/signup</a></p>');
 
-  response.write('<p>Hello World</p>');
+  response.write('<p>Hello Rejekts</p>');
 
   response.write('<p><b>Version:</b> '+process.env.NODE_VERSION+'</p>');
   response.write('<p><b>Background Color:</b> '+process.env.BACKGROUND_COLOR+'</p>');
